4f90b16a81bb6ca2bb6cf37e58f19f62534c915d
Raquel-RP <raquelrp1426@hotmail.com>
2024-04-01 20:03:31 +0200

    ebpf filter

--- filter-program.c
+++ filter-program.c
@@ -1,34 +1,22 @@
 #include <linux/bpf.h>
 #include <linux/if_ether.h>
 #include <linux/ip.h>
+#include <linux/pkt_cls.h>
+#include <bpf/bpf_endian.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
 
-SEC("filter")
+SEC("tc")
+
 int filter_func(struct __sk_buff *skb) {
-    // Obtiene el puntero al encabezado Ethernet
-    void *data = (void *)(long)skb->data;
-    void *data_end = (void *)(long)skb->data_end;
-
-    // Verifica que haya suficiente espacio para el encabezado Ethernet e IP
-    if (data + sizeof(struct ethhdr) + sizeof(struct iphdr) > data_end)
-        return TC_ACT_OK;
-
-    // Obtiene el puntero al encabezado IP
-    struct ethhdr *eth = data;
-    struct iphdr *ip = data + sizeof(struct ethhdr);
 
     // Filtra paquetes marcados con la marca 0x1234
     if (skb->mark == 0x1234) {
-        // Verifica si es un paquete IP
-        if (eth->h_proto == htons(ETH_P_IP)) {
-            // Verifica si es un paquete UDP
-            if (ip->protocol == IPPROTO_UDP) {
-                // Filtra el paquete
-                return TC_ACT_SHOT;
-            }
-        }
+        return TC_ACT_SHOT;
+
     }
 
     return TC_ACT_OK;
 }
 
-char _license[] SEC("license") = "GPL";
+char __license[] SEC("license") = "GPL";
